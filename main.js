82ff5ef0d798d4602ef30a0d26b2d754e9d7954f
mj2068 <yuelianghushenfu@sina.com>
2022-11-04 21:46:42 +0800

    test dom stuff, color, mousemove .etc...

--- main.js
+++ main.js
@@ -1,51 +1,28 @@
 import "./style.css";
-import javascriptLogo from "./javascript.svg";
-import { setupCounter } from "./counter.js";
-import { loremIpsum, LoremIpsum } from "lorem-ipsum";
+import axios from "axios";
+import color from "color";
 
-document.querySelector("#app").innerHTML = `
-  <div>
-
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`;
-
-setupCounter(document.querySelector("#counter"));
-
-console.log(LoremIpsum);
-
-const lorem = new LoremIpsum({
-  sentencesPerParagraph: {
-    max: 8,
-    min: 4,
-  },
-  wordsPerSentence: {
-    max: 16,
-    min: 4,
-  },
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("DOMContentLoaded");
 });
 
-console.log(lorem.generateWords());
-const app = document.querySelector("#app");
+const container = document.getElementById("container");
 
-for (let index = 1; index < 6; index++) {
-  const node = document.createElement("p");
-  node.innerHTML = lorem.generateWords();
-  app.appendChild(node);
-}
+const button1 = document.getElementById("button1");
 
-const node = document.createElement("p");
-node.innerText = lorem.generateSentences(10);
-app.appendChild(node);
+
+button1.addEventListener("click", function () {
+  axios
+    .get("http://localhost")
+    .then(function (response) {
+      console.log(response);
+      if (200 === response.status) {
+        let e = document.createElement("h1");
+        e.innerHTML = response.data;
+        container.appendChild(e);
+      }
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+});

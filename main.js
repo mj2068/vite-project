dc68718ee062368215b567fb1ec2dace6e8fdb54
mj2068 <yuelianghushenfu@sina.com>
2022-11-07 19:12:47 +0800

    i'm in pain!

--- main.js
+++ main.js
@@ -6,13 +6,14 @@ document.addEventListener("DOMContentLoaded", () => {
   console.log("DOMContentLoaded");
 });
 
+const app = document.getElementById("app");
 const container = document.getElementById("container");
 const button1 = document.getElementById("button1");
 
 const data = [];
 
-const kfs = [{}, { opacity: 0 }];
-const timing = { duration: 1000 };
+const kfs = [{ rotate: "360deg", scale: 0, opacity: 0 }];
+const timing = { easing: "cubic-bezier(1,-0.46,.82,0)", duration: 300 };
 
 // document.querySelectorAll(".box1").forEach((element) => {
 //   element.animate(kfs, timing);
@@ -24,14 +25,20 @@ button1.addEventListener("click", function (event) {
   axios
     .get("http://localhost")
     .then(function (response) {
-      console.log(response);
+      // console.log(response);
       if (200 === response.status) {
         let e = document.createElement("h1");
         e.innerHTML = response.data;
         container.appendChild(e);
-        setTimeout(() => {
-          container.removeChild(e);
-        }, 3000);
+        e.animate(
+          { translate: ["0 150px", "0 0"] },
+          { duration: 800, easing: "ease-in" }
+        ).onfinish = () => {
+          setTimeout(() => {
+            e.animate({ translate: ["0 150px"] }, 800).onfinish = () =>
+              e.remove();
+          }, 1000);
+        };
       }
     })
     .catch(function (error) {
@@ -41,18 +48,19 @@ button1.addEventListener("click", function (event) {
 
 document.addEventListener("click", (event) => {
   // append only if it's the body element that's being clicked on
-  if (event.target === document.body) {
+  if (event.target === app) {
     let div = document.createElement("div");
     div.className = "box1";
-    div.style.cssText = `position: absolute; top: ${
-      event.clientY - container.offsetTop - 50
-    }px; left: ${event.clientX - container.offsetLeft - 50}px`;
-    container.append(div);
+    div.style.position = "absolute";
+    div.style.left = event.clientX - app.offsetLeft - 50 + "px";
+    div.style.top = event.clientY - app.offsetTop - 50 + "px";
+    app.append(div);
+    div.animate({ scale: [0, 1] }, { duration: 100, easing: "ease-in" });
   } else if (event.target.classList.contains("box1")) {
-    animatedRemove(event.target);
+    animateThenRemove(event.target);
   }
 });
 
-function animatedRemove(elmt) {
-  elmt.animate(kfs, 500).onfinish = () => elmt.remove();
+function animateThenRemove(e) {
+  e.animate(kfs, timing).onfinish = () => e.remove();
 }

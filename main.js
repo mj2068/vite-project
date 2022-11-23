07a7a02b8389a62bcab8efcb5246bd77937a7524
mj2068 <yuelianghushenfu@sina.com>
2022-11-23 19:45:03 +0800

    multiple nested setTimeout. i'm disgusted

--- main.js
+++ main.js
@@ -1,66 +1,65 @@
 import "./style.css";
 import axios from "axios";
-import color from "color";
-
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("DOMContentLoaded");
-});
+import _ from "lodash";
 
 const app = document.getElementById("app");
-const container = document.getElementById("container");
+const container = document.getElementById("notification-container");
 const button1 = document.getElementById("button1");
 
 const data = [];
-
-const kfs = [{ rotate: "360deg", scale: 0, opacity: 0 }];
-const timing = { easing: "cubic-bezier(1,-0.46,.82,0)", duration: 300 };
-
-// document.querySelectorAll(".box1").forEach((element) => {
-//   element.animate(kfs, timing);
-// });
+let index = 0;
 
 button1.addEventListener("click", function (event) {
-  console.log("button1 click");
+  // console.log("button1 click");
   // event.stopPropagation();
-  axios
-    .get("http://localhost")
-    .then(function (response) {
-      // console.log(response);
-      if (200 === response.status) {
-        let e = document.createElement("h1");
-        e.innerHTML = response.data;
-        container.appendChild(e);
-        e.animate(
-          { translate: ["0 150px", "0 0"] },
-          { duration: 800, easing: "ease-in" }
-        ).onfinish = () => {
-          setTimeout(() => {
-            e.animate({ translate: ["0 150px"] }, 800).onfinish = () =>
-              e.remove();
-          }, 1000);
-        };
-      }
-    })
-    .catch(function (error) {
-      console.error(error);
-    });
+  let n = {};
+  n.id = index++;
+  n.number = Math.random();
+  console.log(n);
+  data.push(n);
 });
 
-document.addEventListener("click", (event) => {
-  // append only if it's the body element that's being clicked on
-  if (event.target === app) {
-    let div = document.createElement("div");
-    div.className = "box1";
-    div.style.position = "absolute";
-    div.style.left = event.clientX - app.offsetLeft - 50 + "px";
-    div.style.top = event.clientY - app.offsetTop - 50 + "px";
-    app.append(div);
-    div.animate({ scale: [0, 1] }, { duration: 100, easing: "ease-in" });
-  } else if (event.target.classList.contains("box1")) {
-    animateThenRemove(event.target);
-  }
-});
+document.addEventListener("DOMContentLoaded", function () {
+  setInterval(() => {
 
-function animateThenRemove(e) {
-  e.animate(kfs, timing).onfinish = () => e.remove();
-}
+
+    console.log("data.length: ", data.length);
+    if (data.length > 0) {
+      let noti = document.createElement("p");
+      noti.classList.add("notification");
+      // noti.style.width = "90%";
+      // noti.style.height = "90%";
+      noti.innerText = data.shift().number;
+
+      container.appendChild(noti);
+
+      const animationToBefore = noti.animate(
+        { top: ["100%", "50%"], translate: ["0 0", "0 70%"] },
+        { duration: 500, fill: "forwards" }
+      );
+
+      animationToBefore.onfinish = () => {
+        setTimeout(() => {
+          // enter to display animation
+          noti.animate(
+            { top: ["50%"], translate: ["0 -50%"] },
+            { duration: 500, fill: "forwards" }
+          ).onfinish = () => {
+            setTimeout(() => {
+              noti.animate(
+                { top: ["50%"], translate: ["0 -170%"] },
+                { duration: 500, fill: "forwards" }
+              ).onfinish = () => {
+                setTimeout(() => {
+                  noti.remove();
+                }, 1000);
+              };
+            }, 1000);
+          };
+        }, 1000);
+      };
+    }
+
+
+  }, 1000);
+});

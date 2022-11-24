9ba2725393c6b9e0ad0386ca1863c820fda73ecf
mj2068 <yuelianghushenfu@sina.com>
2022-11-24 09:27:21 +0800

    success... kinda... :p

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const button1 = document.getElementById("button1");
 let data = [];
 const processingData = [];
 let index = 0;
-let busy = false;
+let busyEnter = false;
 
 // this button is only for push one new random number to the data array
 button1.addEventListener("click", function (event) {
@@ -26,35 +26,48 @@ document.addEventListener("DOMContentLoaded", function () {
   // data acquiring
   setInterval(() => {
     if (data.length > 0) {
-      data.forEach((element) => processingData.push(element));
-      console.log("data pushed");
+      data.forEach((element) => {
+        let notification = makeNotification(element);
+        container.appendChild(notification);
+        processingData.push(notification);
+      });
+      console.log(`${data.length} new data item(s)`);
       data = [];
     }
   }, 1000);
 
   // processing new data
   setInterval(() => {
-    if (processingData.length > 0 && !busy) {
-      console.log(processingData.length)
-      busy = true;
+    if (processingData.length > 0 && !busyEnter) {
+      console.log(processingData.length);
       animateNotification(processingData.shift());
     }
   }, 100);
 });
 
-function animateNotification(dataElement) {
-  let htmlElement = makeHTML(dataElement);
-  container.appendChild(htmlElement);
-  let animation = htmlElement.animate({ translate: ["0 -100%"] }, 1000);
-  animation.addEventListener("finish", (e) => {
-    e.target.effect.target.remove();
-    busy = false;
+function animateNotification(notification) {
+  busyEnter = true;
+  let animation = notification.animate({ translate: ["0 0"] }, 1000);
+  animation.addEventListener("finish", () => {
+    notification.classList.remove("before-enter");
+    notification.classList.add("display");
+    setTimeout(() => {
+      removeNotification(notification);
+      busyEnter = false;
+    }, 2000);
   });
 }
 
-function makeHTML(dataElement, type = "div") {
+function makeNotification(dataElement, type = "div") {
   let div = document.createElement(type);
   div.innerText = dataElement.number;
-  div.classList.add("notification");
+  div.classList.add(...["notification", "before-enter"]);
   return div;
+}
+
+function removeNotification(notification) {
+  let removeAnimation = notification.animate({ translate: ["0 -120%"] }, 1000);
+  removeAnimation.addEventListener("finish", () => {
+    notification.remove();
+  });
 }
